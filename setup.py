8a43a18b9600fd35d5092643847d05248822fef9
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-05-04 14:13:56 -0700

    :boom:

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+setup(
+    name='dcos_test_util',
+    version='0.1',
+    description='DC/OS test orchestration and lower-level cluster interface',
+    url='https://dcos.io',
+    author='Mesosphere, Inc.',
+    author_email='help@dcos.io',
+    license='apache2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=['test_util'],
+    install_requires=[
+        # Pins taken from 'azure==2.0.0rc4'
+        'msrest==0.4.0',
+        'msrestazure==0.4.1',
+        'azure-common==1.1.4',
+        'azure-storage==0.32.0',
+        'azure-mgmt-network==0.30.0rc4',
+        'azure-mgmt-resource==0.30.0rc4',
+        'boto3',
+        'botocore',
+        'pytest',
+        'pyyaml',
+        'requests==2.10.0',
+        'retrying'],
+    package_data={
+        'test_util': [
+            'templates/vpc-cluster-template.json',
+            'templates/vpc-ebs-only-cluster-template.json'
+        ]
+    },
+    zip_safe=False
+)
