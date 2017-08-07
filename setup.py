b7f1a1a2c6ab36351103f8d7a0eea2c0ba864458
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-08-07 10:55:17 -0700

    Bump dcos-test-utils

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         ],
     },
     dependency_links=[
-        'https://github.com/dcos/dcos-test-utils@924d84d8b79f39bf6a1cdd8043b0e65fe9171eec'
+        'https://github.com/dcos/dcos-test-utils@2588a9e79f7eb1b81f37689298b8c83c1f924946'
     ],
     package_data={
         'dcos_launch': [
