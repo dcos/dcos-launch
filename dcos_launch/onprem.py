fc9f097ccb0a62536c77c611c530b25bba58e75c
Maksym Naboka <maksym.naboka@gmail.com>
2017-10-02 11:26:13 -0700

    add override for `fault_domain_enabled`
    set the `fault_domain_enabled` to true, in order to render the fault
    domain script from a passed template.

--- dcos_launch/onprem.py
+++ dcos_launch/onprem.py
@@ -86,6 +86,7 @@ class OnpremLauncher(dcos_launch.util.AbstractLauncher):
             onprem_config['ip_detect_public_contents'] = yaml.dump(pkg_resources.resource_string(
                 'dcos_launch', 'ip-detect/{}_public.sh'.format(self.config['platform'])).decode())
         if 'fault_domain_detect_contents' not in onprem_config:
+            onprem_config['fault_domain_enabled'] = 'true'
             onprem_config['fault_domain_detect_contents'] = yaml.dump(pkg_resources.resource_string(
                 'dcos_launch', 'fault-domain-detect/{}.sh'.format(self.config['platform'])).decode())
         # For no good reason the installer uses 'ip_detect_script' instead of 'ip_detect_contents'
