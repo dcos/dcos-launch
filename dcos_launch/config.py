8af6ac64b7ce3c9197ae861a9f94f89b098ba96a
Margaret Sy <margaretsy1016@gmail.com>
2017-11-07 16:56:19 -0800

    default config bootstrap_os_name to cluster os_name

--- dcos_launch/config.py
+++ dcos_launch/config.py
@@ -265,7 +265,7 @@ AWS_ONPREM_SCHEMA = {
     'bootstrap_os_name': {
         'type': 'string',
         'required': False,
-        'default': 'cent-os-7-dcos-prereqs',
+        'default_setter': lambda doc: doc.get('os_name') or 'cent-os-7-dcos-prereqs',
         'allowed': list(aws.OS_AMIS.keys())},
     'instance_ami': {
         'type': 'string',
