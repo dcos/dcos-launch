8626289692b2b04af130fd0aed7daa98e80e471f
Margaret Sy <margaretsy1016@gmail.com>
2017-12-07 15:57:43 -0800

    try separate handle for reading

--- test/conftest.py
+++ test/conftest.py
@@ -273,10 +273,10 @@ def check_success(capsys, tmpdir, config_path):
     launcher.test([], {})
     # check that option to write test output to file works
     test_log_path = str(tmpdir.join('test_logs.txt'))
-    with open(test_log_path, 'w+') as log_handle:
+    with open(test_log_path, 'w') as log_handle:
         launcher.test([], {}, output=log_handle)
-        logs = log_handle.read()
-        assert logs == 'Hooray, everything worked'
+    with open(test_log_path, 'r') as log_handle:
+        assert log_handle.read() == 'Hooray, everything worked'
 
     launcher.delete()
 
