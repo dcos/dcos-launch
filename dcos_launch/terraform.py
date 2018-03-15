6cdcf807d27506cb51c183c9c0d7e59ad47bcb9a
Charles Provencher <cprovencher@mesosphere.com>
2018-03-15 16:22:29 -0700

    Add Terraform dependency management and checks

--- dcos_launch/terraform.py
+++ dcos_launch/terraform.py
@@ -1,10 +1,14 @@
 import json
+import logging
 import os
 import re
 import shutil
 import subprocess
 import uuid
+import zipfile
 from functools import wraps
+
+import requests
 
 import dcos_launch.config
 import yaml
@@ -12,6 +16,7 @@ from dcos_launch import gcp, util
 from dcos_launch.platforms import aws
 from dcos_test_utils import helpers
 
+log = logging.getLogger(__name__)
 IP_REGEX = '(\d{1,3}.){3}\d{1,3}'
 IP_LIST_REGEX = '\[[^\]]+\]'
 
@@ -40,7 +45,7 @@ def _convert_to_describe_format(ips: list) -> list:
     return [{'private_ip': None, 'public_ip': ip} for ip in ips]
 
 
-def teardown(f):
+def catch_failed_init(f):
     @wraps(f)
     def handle_exception(*args, **kwargs):
         try:
@@ -58,15 +63,38 @@ class TerraformLauncher(util.AbstractLauncher):
         self.config = config
         self.init_dir = dcos_launch.config.expand_path('', self.config['init_dir'])
         self.cluster_profile_path = os.path.join(self.init_dir, 'desired_cluster_profile.tfvars')
+        self.priv_key_file = None
+        self.dcos_launch_root_dir = os.path.dirname(os.path.join(os.path.abspath(__file__), '..'))
+        self.terraform_binary = os.path.join(self.dcos_launch_root_dir, 'terraform')
+
+    def terraform_cmd(self):
+        """ Returns the right Terraform invocation command depending on whether it was installed by the user or by
+        dcos-launch.
+        """
+        binary = self.terraform_binary
+        if not os.path.exists(binary):
+            binary = 'terraform'
+        return binary
 
     def create(self):
-        self.key_helper()
         # create terraform directory
         if os.path.exists(self.init_dir):
             raise util.LauncherError('ClusterAlreadyExists', "Either the cluster you are trying to create is already "
                                                              "running or the init_dir you specified in your config is "
                                                              "already used by another active cluster.")
+        # TODO if any of the below fails, teardown?
         os.makedirs(self.init_dir)
+
+        # Check if Terraform is installed by running 'terraform version'. If that fails, install Terraform.
+        try:
+            subprocess.run([self.terraform_cmd(), 'version'], check=True, stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+        except FileNotFoundError:
+            log.info('No Terraform installation detected. Terraform is now being installed.')
+            self._install_terraform()
+
+        # TODO if any of the below fails, ssh-add -d?
+        self.key_helper()
         module = 'github.com/dcos/{}?ref={}/{}'.format(
             'terraform-dcos-enterprise' if self.config['dcos-enterprise'] else 'terraform-dcos',
             self.config['terraform_dcos_enterprise_version'] if self.config['dcos-enterprise'] else
@@ -82,12 +110,26 @@ class TerraformLauncher(util.AbstractLauncher):
                 else:
                     file.write('"{}"\n'.format(v))
 
-        subprocess.run(['terraform', 'init', '-from-module', module], cwd=self.init_dir, check=True,
+        # TODO if any of the below fails, terraform destroy?
+        subprocess.run([self.terraform_cmd(), 'init', '-from-module', module], cwd=self.init_dir, check=True,
                        stderr=subprocess.STDOUT)
-        subprocess.run(['terraform', 'apply', '-auto-approve', '-var-file', self.cluster_profile_path],
+        subprocess.run([self.terraform_cmd(), 'apply', '-auto-approve', '-var-file', self.cluster_profile_path],
                        cwd=self.init_dir, check=True, stderr=subprocess.STDOUT)
 
         return self.config
+
+    def _install_terraform(self):
+        download_path = 'terraform.tar.gz'
+        with open(download_path, 'wb') as f:
+            log.info('Downloading...')
+            r = requests.get(self.config['terraform_tarball_url'])
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+        with zipfile.ZipFile(download_path, 'r') as tfm_zip:
+            tfm_zip.extractall(self.dcos_launch_root_dir)
+        os.chmod(self.terraform_binary, 0o100)
+        os.remove(download_path)
+        log.info('Terraform installation complete.')
 
     def wait(self):
         """ Nothing to do here because unlike the other launchers, create() runs and also waits for a subprocess to
@@ -96,9 +138,12 @@ class TerraformLauncher(util.AbstractLauncher):
         pass
 
     def delete(self):
-        subprocess.run(['terraform', 'destroy', '-force', '-var-file', self.cluster_profile_path], cwd=self.init_dir,
-                       check=True, stderr=subprocess.STDOUT)
+        subprocess.run([self.terraform_cmd(), 'destroy', '-force', '-var-file', self.cluster_profile_path],
+                       cwd=self.init_dir, check=True, stderr=subprocess.STDOUT)
         shutil.rmtree(self.init_dir, ignore_errors=True)
+        if self.priv_key_file:
+            subprocess.run(['ssh-add', '-d', self.priv_key_file], check=False,
+                           stderr=subprocess.STDOUT)
 
     def describe(self) -> dict:
         """ Output example
@@ -117,8 +162,7 @@ class TerraformLauncher(util.AbstractLauncher):
             35.197.122.209
         ]
         Public Agent ELB Address = 35.230.52.188"""
-        result = subprocess.run(['terraform', 'show'], cwd=self.init_dir, check=True, stdout=subprocess.PIPE)
-        # unescape line endings
+        result = subprocess.run([self.terraform_cmd(), 'show'], cwd=self.init_dir, check=True, stdout=subprocess.PIPE)
         info = result.stdout.decode('utf-8')
         # crop the output to speed up regex
         info = info[info.find('Outputs:'):]
@@ -146,9 +190,22 @@ class TerraformLauncher(util.AbstractLauncher):
 
         return description
 
+    def _ssh_add(self, private_key: bytes):
+        self.priv_key_file = helpers.session_tempfile(private_key)
+        os.chmod(self.priv_key_file, 0o600)
+        try:
+            if 'SSH_AUTH_SOCK' not in os.environ:
+                log.info('No ssh-agent running. Starting one...')
+                subprocess.run(['eval', '`ssh-agent -s`'], check=True, stderr=subprocess.STDOUT)
+            subprocess.run(['ssh-add', self.priv_key_file], check=True, stderr=subprocess.STDOUT)
+        except Exception as e:
+            raise util.LauncherError('KeyHelperFailed', "Make sure you have your operating system's keychain package "
+                                                        "installed (e.g. 'brew install keychain' or 'apt-get install "
+                                                        "keychain').")
+
 
 class GcpLauncher(TerraformLauncher):
-    @teardown
+    @catch_failed_init
     def __init__(self, config: dict, env=None):
         super().__init__(config, env)
         # if gcp region is nowhere to be found, the default value in terraform-dcos will be used
@@ -165,15 +222,13 @@ class GcpLauncher(TerraformLauncher):
     def key_helper(self):
         if self.config['key_helper'] or 'gcp_ssh_pub_key_file' not in self.config['terraform_config']:
             private_key, public_key = util.generate_rsa_keypair()
-            priv_key_file = helpers.session_tempfile(private_key)
-            os.chmod(priv_key_file, 0o600)
+            self._ssh_add(private_key)
             pub_key_file = helpers.session_tempfile(public_key)
-            subprocess.run(['ssh-add', priv_key_file], check=True, stderr=subprocess.STDOUT)
             self.config['terraform_config']['gcp_ssh_pub_key_file'] = pub_key_file
 
 
 class AzureLauncher(TerraformLauncher):
-    @teardown
+    @catch_failed_init
     def __init__(self, config: dict, env=None):
         super().__init__(config, env)
         dcos_launch.util.set_from_env('AZURE_SUBSCRIPTION_ID')
@@ -187,14 +242,12 @@ class AzureLauncher(TerraformLauncher):
     def key_helper(self):
         if self.config['key_helper'] or 'ssh_pub_key' not in self.config['terraform_config']:
             private_key, public_key = util.generate_rsa_keypair()
-            priv_key_file = helpers.session_tempfile(private_key)
-            os.chmod(priv_key_file, 0o600)
-            subprocess.run(['ssh-add', priv_key_file], check=True, stderr=subprocess.STDOUT)
+            self._ssh_add(private_key)
             self.config['terraform_config']['ssh_pub_key'] = public_key.decode('utf-8')
 
 
 class AwsLauncher(TerraformLauncher):
-    @teardown
+    @catch_failed_init
     def __init__(self, config: dict, env=None):
         super().__init__(config, env)
         creds_file = '~/.aws/credentials'
@@ -210,7 +263,5 @@ class AwsLauncher(TerraformLauncher):
             bw = aws.BotoWrapper(self.config['aws_region'])
             key_name = 'terraform-dcos-launch-' + str(uuid.uuid4())
             private_key = bw.create_key_pair(key_name)
-            priv_key_file = helpers.session_tempfile(private_key)
-            os.chmod(priv_key_file, 0o600)
-            subprocess.run(['ssh-add', priv_key_file], check=True, stderr=subprocess.STDOUT)
+            self._ssh_add(private_key)
             self.config['terraform_config']['ssh_key_name'] = key_name
