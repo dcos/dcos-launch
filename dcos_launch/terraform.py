6e64b8164faee4d640cd2c47fa5b8dbfc44a8f0f
Charles Provencher <cprovencher@mesosphere.com>
2018-03-21 16:48:24 -0700

    Make 'dcos-launch pytest' work for terraform

--- dcos_launch/terraform.py
+++ dcos_launch/terraform.py
@@ -6,41 +6,39 @@ import shutil
 import subprocess
 import uuid
 import zipfile
-
 import requests
-
+from dcos_test_utils import helpers
 import dcos_launch.config
 import yaml
 from dcos_launch import gcp, util
 from dcos_launch.platforms import aws
-from dcos_test_utils import helpers
 
 log = logging.getLogger(__name__)
 IP_REGEX = '(\d{1,3}.){3}\d{1,3}'
 IP_LIST_REGEX = '\[[^\]]+\]'
 
 
-def _get_ips(*arg, info: str) -> list:
+def _get_ips(prefix: str, info: str) -> list:
     ips = []
-    for prefix in arg:
-        prefix += ' = '
-        m = re.search('{}{}'.format(prefix, IP_REGEX), info)
+    prefix += ' = '
+    m = re.search('{}{}'.format(prefix, IP_REGEX), info)
+    if m:
+        ips = [m.group(0)[len(prefix):]]
+    else:
+        m = re.search('{}{}'.format(prefix, IP_LIST_REGEX), info)
         if m:
-            ips.append(m.group(0)[len(prefix):])
-        else:
-            m = re.search('{}{}'.format(prefix, IP_LIST_REGEX), info)
-            if m:
-                # remove prefix
-                s = m.group(0)[len(prefix):]
-                # remove whitespace
-                s = "".join(s.split())
-                # remove brackets
-                s = s[1:-1]
-                ips += s.split(',')
+            # remove prefix
+            s = m.group(0)[len(prefix):]
+            # remove whitespace
+            s = "".join(s.split())
+            # remove brackets
+            s = s[1:-1]
+            ips = s.split(',')
     return ips
 
 
 def _convert_to_describe_format(ips: list) -> list:
+    # TODO private ips not currently outputted in terraform-dcos
     return [{'private_ip': None, 'public_ip': ip} for ip in ips]
 
 
@@ -51,7 +49,7 @@ class TerraformLauncher(util.AbstractLauncher):
         self.config = config
         self.init_dir = dcos_launch.config.expand_path('', self.config['init_dir'])
         self.cluster_profile_path = os.path.join(self.init_dir, 'desired_cluster_profile.tfvars')
-        self.dcos_launch_root_dir = os.path.abspath(os.path.join(__file__, '..'))
+        self.dcos_launch_root_dir = os.path.abspath(os.path.join(self.init_dir, '..'))
         self.terraform_binary = os.path.join(self.dcos_launch_root_dir, 'terraform')
         self.default_priv_key_path = os.path.join(self.init_dir, 'key.pem')
 
@@ -69,58 +67,47 @@ class TerraformLauncher(util.AbstractLauncher):
             raise util.LauncherError('ClusterAlreadyExists', "Either the cluster you are trying to create is already "
                                                              "running or the init_dir you specified in your config is "
                                                              "already used by another active cluster.")
+        os.makedirs(self.init_dir)
+        # Check if Terraform is installed by running 'terraform version'. If that fails, install Terraform.
         try:
-            os.makedirs(self.init_dir)
-            # Check if Terraform is installed by running 'terraform version'. If that fails, install Terraform.
-            try:
-                subprocess.run([self.terraform_cmd(), 'version'], check=True, stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT)
-            except FileNotFoundError:
-                log.info('No Terraform installation detected. Terraform is now being installed.')
-                self._install_terraform()
+            subprocess.run([self.terraform_cmd(), 'version'], check=True, stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+        except FileNotFoundError:
+            log.info('No Terraform installation detected. Terraform is now being installed.')
+            self._install_terraform()
 
-            try:
-                if self.config['key_helper']:
-                    self.key_helper()
-                module = 'github.com/dcos/{}?ref={}/{}'.format(
-                    'terraform-dcos-enterprise' if self.config['dcos-enterprise'] else 'terraform-dcos',
-                    self.config['terraform_dcos_enterprise_version'] if self.config['dcos-enterprise'] else
-                    self.config['terraform_dcos_version'], self.config['platform'])
+        if self.config['key_helper']:
+            self.key_helper()
 
-                # Converting our YAML config to the required format. You can find an example of that format in the
-                # Advance YAML Configuration" section here:
-                # https://github.com/mesosphere/terraform-dcos-enterprise/tree/master/aws
-                with open(self.cluster_profile_path, 'w') as file:
-                    for k, v in self.config['terraform_config'].items():
-                        file.write(k + ' = ')
-                        if type(k) is dict:
-                            file.write('<<EOF\n{}\nEOF\n'.format(yaml.dump(v)))
-                        else:
-                            file.write('"{}"\n'.format(v))
-                try:
-                    subprocess.run([self.terraform_cmd(), 'init', '-from-module', module], cwd=self.init_dir,
-                                   check=True, stderr=subprocess.STDOUT)
-                    ssh_cmd, shell = self._ssh_agent_setup()
-                    subprocess.run(ssh_cmd + [self.terraform_cmd(), 'apply', '-auto-approve', '-var-file',
-                                   self.cluster_profile_path], cwd=self.init_dir, check=True,
-                                   stderr=subprocess.STDOUT, shell=shell)
-                except Exception as e:
-                    self._delete_cluster()
-                    raise e
-            except Exception as e:
-                self._remove_ssh_key_from_agent()
-                raise e
-        except Exception as e:
-            self._remove_init_dir()
-            raise e
+        module = 'github.com/dcos/{}?ref={}/{}'.format(
+            'terraform-dcos-enterprise' if self.config['dcos-enterprise'] else 'terraform-dcos',
+            self.config['terraform_dcos_enterprise_version'] if self.config['dcos-enterprise'] else
+            self.config['terraform_dcos_version'], self.config['platform'])
 
+        # Converting our YAML config to the required format. You can find an example of that format in the
+        # Advance YAML Configuration" section here:
+        # https://github.com/mesosphere/terraform-dcos-enterprise/tree/master/aws
+        with open(self.cluster_profile_path, 'w') as file:
+            for k, v in self.config['terraform_config'].items():
+                file.write(k + ' = ')
+                if type(k) is dict:
+                    file.write('<<EOF\n{}\nEOF\n'.format(yaml.dump(v)))
+                else:
+                    file.write('"{}"\n'.format(v))
+        subprocess.run([self.terraform_cmd(), 'init', '-from-module', module], cwd=self.init_dir,
+                       check=True, stderr=subprocess.STDOUT)
+        cmd = [self.terraform_cmd(), 'apply', '-auto-approve', '-var-file', self.cluster_profile_path]
+        ssh_cmd, shell = self._get_full_ssh_agent_cmd(['ssh-add', self.config['ssh_private_key_filename'], '&&'])
+        cmd = ssh_cmd + cmd
+        cmd = ' '.join(cmd) if shell else cmd
+        subprocess.run(cmd, cwd=self.init_dir, check=True, stderr=subprocess.STDOUT, shell=shell,
+                       env=os.environ)
         return self.config
 
-    def _ssh_agent_setup(self):
+    def _get_full_ssh_agent_cmd(self, cmd: list):
         if not self.config['key_helper']:
             return [], False
         shell = False
-        cmd = ['ssh-add', self.config['ssh_private_key_filename'], '&&']
         if 'SSH_AUTH_SOCK' not in os.environ:
             cmd = ['eval', '`ssh-agent -s`', '&&'] + cmd
             shell = True
@@ -149,89 +136,113 @@ class TerraformLauncher(util.AbstractLauncher):
         pass
 
     def delete(self):
-        self._delete_cluster()
-        self._remove_ssh_key_from_agent()
-        self._remove_init_dir()
-
-    def _delete_cluster(self):
+        # delete the cluster
         subprocess.run([self.terraform_cmd(), 'destroy', '-force', '-var-file', self.cluster_profile_path],
-                       cwd=self.init_dir, check=True, stderr=subprocess.STDOUT)
-
-    def _remove_init_dir(self):
+                       cwd=self.init_dir, check=True, stderr=subprocess.STDOUT, env=os.environ)
+        # remove the key from the ssh-agent
+        cmd, shell = self._get_full_ssh_agent_cmd(['ssh-add', '-d', self.config['ssh_private_key_filename']])
+        cmd = ' '.join(cmd) if shell else cmd
+        subprocess.run(cmd, check=False, stderr=subprocess.STDOUT, shell=shell)
+        # remove the init dir
         shutil.rmtree(self.init_dir, ignore_errors=True)
 
-    def _remove_ssh_key_from_agent(self):
-        if 'SSH_AUTH_SOCK' in os.environ:
-            subprocess.run(['ssh-add', '-d', self.config['ssh_private_key_filename']], check=False,
-                           stderr=subprocess.STDOUT)
-
     def describe(self) -> dict:
-        """ Output example
-        Outputs:
-
-        Bootstrap Public IP Address = 35.230.27.229
-        Master ELB Address = 35.230.34.40
-        Mesos Agent Public IP = [
-            35.230.112.212,
-            35.230.68.63
+        """ Sample output from 'terraform output' command:
+        Bootstrap Host Public IP = 35.227.147.39
+        Master ELB Public IP = 35.230.64.74
+        Master Public IPs = [
+            35.230.56.247
         ]
-        Mesos Master Public IP = [
-            35.230.48.204
+        Private Agent Public IPs = [
+            35.230.42.164,
+            35.203.136.227
         ]
-        Mesos Public Agent Public IP = [
-            35.197.122.209
+        Public Agent ELB Public IP = 35.230.71.57
+        Public Agent Public IPs = [
+            35.230.88.105
         ]
-        Public Agent ELB Address = 35.230.52.188"""
-        result = subprocess.run([self.terraform_cmd(), 'show'], cwd=self.init_dir, check=True, stdout=subprocess.PIPE)
+        ssh_user = core
+        """
+        result = subprocess.run([self.terraform_cmd(), 'output'], cwd=self.init_dir, check=True, stdout=subprocess.PIPE)
         info = result.stdout.decode('utf-8')
-        # crop the output to speed up regex
-        info = info[info.find('Outputs:'):]
+        self.config['ssh_user'] = re.search('ssh_user = \w+', info).group(0)[len('ssh_user = '):]
 
-        private_agents_ips = _convert_to_describe_format(
-            _get_ips('Mesos Agent Public IP', 'Private Agent Public IP Address', info=info))
-        private_agents_gpu_addresses = _get_ips('GPU Private Public IP Address', info=info)
+        private_agents_ips = _convert_to_describe_format(_get_ips('Private Agent Public IPs', info))
+        private_agents_gpu_addresses = _get_ips('GPU Public IPs', info)
         for i in range(len(private_agents_gpu_addresses)):
-            private_agents_ips[i]['GPU_address'] = private_agents_gpu_addresses[i]
+            private_agents_ips[i]['GPU Public IPs'] = private_agents_gpu_addresses[i]
 
         description = {
-            'bootstrap_host': _convert_to_describe_format(_get_ips('Bootstrap Public IP Address', info=info)),
-            'masters': _convert_to_describe_format(_get_ips('Mesos Master Public IP', info=info)),
+            'bootstrap_host': _convert_to_describe_format(_get_ips('Bootstrap Host Public IP', info=info)),
+            'masters': _convert_to_describe_format(_get_ips('Master Public IPs', info=info)),
             'private_agents': private_agents_ips,
-            'public_agents': _convert_to_describe_format(_get_ips('Public Agent Public IP Address',
-                                                                  'Mesos Public Agent Public IP', info=info))}
+            'public_agents': _convert_to_describe_format(_get_ips('Public Agent Public IPs', info))}
 
-        master_elb_address = _get_ips('Master ELB Address', info=info)
-        public_agent_elb_address = _get_ips('Public Agent ELB Address', info=info)
+        master_elb_address = _get_ips('Master ELB Public IP', info=info)
+        public_agent_elb_address = _get_ips('Public Agent ELB Public IP', info=info)
 
         if master_elb_address:
-            description.update({'master_elb_address': master_elb_address[0]})
+            description.update({'Master ELB Public IP': master_elb_address[0]})
         if public_agent_elb_address:
-            description.update({'public_agent_elb_address': public_agent_elb_address[0]})
+            description.update({'Public Agent ELB Public IP': public_agent_elb_address[0]})
 
         return description
 
     def key_helper(self):
         private_key, public_key = util.generate_rsa_keypair()
+        self.config['ssh_private_key'] = private_key.decode('utf-8')
         with open(self.default_priv_key_path, 'wb') as f:
             f.write(private_key)
         os.chmod(self.default_priv_key_path, 0o600)
         self.config['ssh_private_key_filename'] = self.default_priv_key_path
         return public_key
 
+    def test(self, args: list, env_dict: dict, test_host: str=None, test_port: int=22, details: dict=None) -> int:
+        # TODO only reason this exists is because private IPs are not yet returned from describe(), which are required
+        # by the parent test() function
+        """ Connects to master host with SSH and then run the internal integration test
+
+        Args:
+            args: a list of args that will follow the py.test command
+            env_dict: the env to use during the test
+        """
+        if args is None:
+            args = list()
+        if self.config['ssh_private_key'] == util.NO_TEST_FLAG or 'ssh_user' not in self.config:
+            raise util.LauncherError('MissingInput', 'DC/OS Launch is missing sufficient SSH info to run tests!')
+        if details is None:
+            details = self.describe()
+        # check for any environment variables that contain spaces
+        env_dict = {e: "'{}'".format(env_dict[e]) if ' ' in env_dict[e] else env_dict[e] for e in env_dict}
+        env_string = ' '.join(['{}={}'.format(e, env_dict[e]) for e in env_dict])
+        arg_string = ' '.join(args)
+        # To support 1.8.9-EE, try using the dcos-integration-test-ee folder if possible
+        pytest_cmd = """ "source /opt/mesosphere/environment.export &&
+cd `find /opt/mesosphere/active/ -name dcos-integration-test* | sort | tail -n 1` &&
+{env} py.test {args}" """.format(env=env_string, args=arg_string)
+        log.info('Running integration test...')
+        if test_host is None:
+            test_host = details['masters'][0]['public_ip']
+        if ':' in test_host:
+            test_host, test_port = test_host.split(':')
+        env_dict['DCOS_DNS_ADDRESS'] = 'http://' + test_host
+        return util.try_to_output_unbuffered(self.config, test_host, pytest_cmd, test_port)
+
 
 class GcpLauncher(TerraformLauncher):
-    def __init__(self, config: dict, env=None):
-        super().__init__(config, env)
+    def create(self):
         # if gcp region is nowhere to be found, the default value in terraform-dcos will be used
-        if 'gcp_region' not in self.config['terraform_config'] and 'GCE_ZONE' in os.environ:
-            self.config['terraform_config']['gcp_region'] = util.set_from_env('GCE_ZONE')
-
-        creds_string, creds_path = gcp.get_credentials(env)
-        if not creds_path:
-            creds_path = helpers.session_tempfile(creds_string)
-        config['terraform_config']['gcp_credentials_key_file'] = creds_path
-        if 'gcp_project' not in config['terraform_config']:
-            config['terraform_config']['gcp_project'] = json.loads(creds_string)['project_id']
+        if 'gcp_zone' not in self.config['terraform_config'] and 'GCE_ZONE' in os.environ:
+            self.config['terraform_config']['gcp_zone'] = util.set_from_env('GCE_ZONE')
+        if 'gcp_credentials_key_file' not in self.config['terraform_config']:
+            creds_string, creds_path = gcp.get_credentials(os.environ)
+            if not creds_path:
+                creds_path = helpers.session_tempfile(creds_string)
+            self.config['terraform_config']['gcp_credentials_key_file'] = creds_path
+        if 'gcp_project' not in self.config['terraform_config']:
+            with open(self.config['terraform_config']['gcp_credentials_key_file']) as f:
+                self.config['terraform_config']['gcp_project'] = json.load(f)['project_id']
+        return super().create()
 
     def key_helper(self):
         if 'gcp_ssh_pub_key_file' not in self.config['terraform_config'] or \
@@ -244,15 +255,15 @@ class GcpLauncher(TerraformLauncher):
 
 
 class AzureLauncher(TerraformLauncher):
-    def __init__(self, config: dict, env=None):
-        super().__init__(config, env)
-        dcos_launch.util.set_from_env('AZURE_SUBSCRIPTION_ID')
-        dcos_launch.util.set_from_env('AZURE_CLIENT_ID')
-        dcos_launch.util.set_from_env('AZURE_CLIENT_SECRET')
-        dcos_launch.util.set_from_env('AZURE_TENANT_ID')
+    def create(self):
+        dcos_launch.util.set_from_env('ARM_SUBSCRIPTION_ID')
+        dcos_launch.util.set_from_env('ARM_CLIENT_ID')
+        dcos_launch.util.set_from_env('ARM_CLIENT_SECRET')
+        dcos_launch.util.set_from_env('ARM_TENANT_ID')
         # if azure region is nowhere to be found, the default value in terraform-dcos will be used
         if 'azure_region' not in self.config['terraform_config'] and 'AZURE_LOCATION' in os.environ:
             self.config['terraform_config']['azure_region'] = util.set_from_env('AZURE_LOCATION')
+        return super().create()
 
     def key_helper(self):
         if 'ssh_pub_key' not in self.config['terraform_config'] or \
@@ -269,6 +280,7 @@ class AwsLauncher(TerraformLauncher):
             key_name = 'terraform-dcos-launch-' + str(uuid.uuid4())
             private_key = bw.create_key_pair(key_name)
             with open(self.default_priv_key_path, 'wb') as f:
-                f.write(private_key)
+                f.write(private_key.encode())
+            os.chmod(self.default_priv_key_path, 0o600)
             self.config['ssh_private_key_filename'] = self.default_priv_key_path
             self.config['terraform_config']['ssh_key_name'] = key_name
