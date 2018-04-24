1ef1d929439c7cc6e8951b6b56ac01f5bab5c1ef
Margaret Sy <margaretsy1016@gmail.com>
2018-04-23 18:56:46 -0700

    add the rest of update states to delete transition states

--- dcos_launch/aws.py
+++ dcos_launch/aws.py
@@ -89,7 +89,13 @@ class DcosCloudformationLauncher(dcos_launch.util.AbstractLauncher):
             # must wait for stack to be deleted before removing
             # network resources on which it depends
             self.stack.wait_for_complete(
-                transition_states=['CREATE_COMPLETE', 'UPDATE_COMPLETE', 'DELETE_IN_PROGRESS'],
+                transition_states=[
+                    'CREATE_COMPLETE',
+                    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
+                    'UPDATE_IN_PROGRESS',
+                    'UPDATE_COMPLETE',
+                    'DELETE_IN_PROGRESS'
+                ],
                 end_states=['DELETE_COMPLETE'])
             self.delete_temp_resources(self.config['temp_resources'])
 
