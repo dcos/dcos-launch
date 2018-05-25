7b3eaa9c478115538ae0f499b29c671e0b52a270
Charles Provencher <cpro1245@gmail.com>
2018-05-25 11:14:50 -0700

    Revert "Sync dcos-launch with latest acs-engine changes"

--- dcos_launch/acs_engine.py
+++ dcos_launch/acs_engine.py
@@ -31,7 +31,6 @@ def generate_acs_engine_template(
         windows_admin_user: str,
         windows_admin_password: str,
         linux_admin_user: str,
-        acs_engine_dcos_orchestrator_release: str,
         ):
     """ Generates the template provided to ACS-engine
     """
@@ -41,7 +40,7 @@ def generate_acs_engine_template(
         "properties": {
             "orchestratorProfile": {
                 "orchestratorType": "DCOS",
-                "orchestratorRelease": acs_engine_dcos_orchestrator_release
+                "orchestratorVersion": "1.11.0"
             },
             "masterProfile": {
                 "count": num_masters,
@@ -67,7 +66,7 @@ def generate_acs_engine_template(
                     "name": "linpub",
                     "count": num_linux_public_agents,
                     "vmSize": linux_public_vm_size,
-                    "osType": "Linux",
+                    "osType": "linux",
                     "dnsPrefix": "linpub" + unique_id,
                     "ports": [80, 443, 22]
                 },
@@ -75,7 +74,7 @@ def generate_acs_engine_template(
                     "name": "linpri",
                     "count": num_linux_private_agents,
                     "vmSize": linux_private_vm_size,
-                    "osType": "Linux"
+                    "osType": "linux"
                 }
             ],
             "windowsProfile": {
@@ -123,6 +122,8 @@ def run_acs_engine(acs_engine_url: str, acs_engine_template):
     cluster_name = acs_engine_template['properties']['masterProfile']['dnsPrefix']
     with open(os.path.join(tmpdir, '_output/{}/azuredeploy.json'.format(cluster_name)), 'r') as f:
         arm_template = json.load(f)
+    arm_template['variables']['agentWindowsOffer'] = 'WindowsServerSemiAnnual'
+    arm_template['variables']['agentWindowsSku'] = 'Datacenter-Core-1709-with-Containers-smalldisk'
     with open(os.path.join(tmpdir, '_output/{}/azuredeploy.parameters.json'.format(cluster_name)), 'r') as f:
         arm_template_parameters_raw = json.load(f)
     arm_template_parameters = dict()
@@ -172,20 +173,10 @@ class ACSEngineLauncher(dcos_launch.util.AbstractLauncher):
             self.config['linux_public_vm_size'],
             self.config['windows_admin_user'],
             self.config['windows_admin_password'],
-            self.config['linux_admin_user'],
-            self.config['acs_engine_dcos_orchestrator_release'])
+            self.config['linux_admin_user'])
         windows_image_source_url = self.config.get('windows_image_source_url')
         if windows_image_source_url:
             acs_engine_template["properties"]["windowsProfile"]["WindowsImageSourceUrl"] = windows_image_source_url
-        else:
-            # Use the official Azure image if a custom VHD was not specified
-            # via the windows_image_source_url config option
-            windows_publisher = self.config.get('windows_publisher')
-            windows_offer = self.config.get('windows_offer')
-            windows_sku = self.config.get('windows_sku')
-            acs_engine_template["properties"]["windowsProfile"]["WindowsPublisher"] = windows_publisher
-            acs_engine_template["properties"]["windowsProfile"]["WindowsOffer"] = windows_offer
-            acs_engine_template["properties"]["windowsProfile"]["WindowsSku"] = windows_sku
         linux_bs_url = self.config.get('dcos_linux_bootstrap_url')
         arm_template, self.config['template_parameters'] = run_acs_engine(self.config['acs_engine_tarball_url'], acs_engine_template)  # noqa
         if linux_bs_url:
