24dc2ef860ca7a491bec4d106580c475ddd28ecc
Charles Provencher <cprovencher@mesosphere.com>
2018-08-21 15:44:25 -0700

    coreos sshd always restart policy

--- dcos_launch/scripts/run_coreos_prereqs.sh
+++ dcos_launch/scripts/run_coreos_prereqs.sh
@@ -1,14 +1,15 @@
 # This works around https://github.com/coreos/bugs/issues/426
 sudo mkdir -p /etc/systemd/system/sshd.service.d
 sudo touch /etc/systemd/system/sshd.service.d/override.conf
-sudo echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
-sudo echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
+sudo bash -c "echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf"
+sudo bash -c "echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf"
 # Without this we get:
 # sshd.service: Start request repeated too quickly.
 # Failed to start OpenSSH server daemon.
 # sshd.service: Unit entered failed state.
 # sshd.service: Failed with result 'start-limit-hit'.
-sudo echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
+sudo bash -c "echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf"
+sudo systemctl restart sshd
 sudo systemctl disable locksmithd
 sudo systemctl stop locksmithd
 sudo systemctl mask locksmithd
