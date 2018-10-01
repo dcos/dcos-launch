aacc759b4f08819e03e35c08f6670055f3e7560a
Charles Provencher <cpro1245@gmail.com>
2018-10-01 12:24:47 -0700

    Update run_centos74_prereqs.sh to install Docker 17.05 (#180)

--- dcos_launch/scripts/run_centos74_prereqs.sh
+++ dcos_launch/scripts/run_centos74_prereqs.sh
@@ -1,23 +1,26 @@
-sudo setenforce 0 && \
-sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-# sudo yum -y update --exclude="docker-engine*"
+sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --enable docker-ce-edge
+
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
 [Service]
 ExecStart=
 ExecStart=/usr/bin/docker daemon --storage-driver=overlay
 EOF
-sudo yum install -y docker-engine-1.13.1
+
+sudo yum install -y --setopt=obsoletes=0 \
+  docker-ce-17.05.0.ce-1.el7.centos \
+  docker-ce-selinux-17.05.0.ce-1.el7.centos
+
 sudo systemctl start docker
 sudo systemctl enable docker
+
 sudo yum install -y wget
 sudo yum install -y git
 sudo yum install -y unzip
@@ -30,3 +33,4 @@ sudo systemctl start ntpd
 sudo getent group nogroup || sudo groupadd nogroup
 sudo getent group docker || sudo groupadd docker
 sudo touch /opt/dcos-prereqs.installed
+
