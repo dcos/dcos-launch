67379357c1f9a02805c7705f5c904afa973af9b0
Charles Provencher <cprovencher@mesosphere.com>
2018-10-04 16:36:46 -0700

    Remove SELinux disabling in prereqs

--- dcos_launch/scripts/run_centos74_prereqs.sh
+++ dcos_launch/scripts/run_centos74_prereqs.sh
@@ -1,5 +1,3 @@
-sudo setenforce 0 && \
-sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
 name=Docker Repository
