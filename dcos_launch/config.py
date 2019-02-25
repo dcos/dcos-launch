39a974e100fc89b0ade92acd59e5549ae140d4f5
Charles Provencher <cpro1245@gmail.com>
2019-02-25 15:27:21 -0800

    Revert "Update default bootstrap_instance_type to m5.xlarge (#218)"
    This reverts commit e5f2623b0ab6df1f69b2a5bcf195eae6b7312b49.

--- dcos_launch/config.py
+++ dcos_launch/config.py
@@ -439,7 +439,7 @@ AWS_ONPREM_SCHEMA = {
     'bootstrap_instance_type': {
         'type': 'string',
         'required': True,
-        'default': 'm5.xlarge'},
+        'default': 'm4.xlarge'},
     'bootstrap_instance_device_name': {
         'type': 'string',
         'required': True,
