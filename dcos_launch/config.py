c2b2a4ad9b3cc39bdee9133b1d035095502ae440
Charles Provencher <cprovencher@mesosphere.com>
2019-03-12 21:15:16 -0700

    Set dcos_config platform to global config platform by default

--- dcos_launch/config.py
+++ dcos_launch/config.py
@@ -118,6 +118,8 @@ def get_validated_config(user_config: dict, config_dir: str) -> dict:
     provider = validator.normalized(user_config)['provider']
     if provider == 'onprem':
         validator.schema.update(ONPREM_DEPLOY_COMMON_SCHEMA)
+        user_config.setdefault('dcos_config', {})
+        user_config['dcos_config'].setdefault('platform', user_config['platform'])
     elif provider == 'terraform':
         validator.schema.update(TERRAFORM_COMMON_SCHEMA)
     elif provider in ('aws', 'azure'):
